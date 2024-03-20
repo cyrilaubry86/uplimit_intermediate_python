9b257c95f81aa163ba7c3f8a2006792b09c95410
cyrilaubry86 <cyrilaubry22@gmail.com>
2024-03-20 16:40:01 +0100

    w4

--- w4/logger_config.py
+++ w4/logger_config.py
@@ -20,35 +20,36 @@ class Logger:
         # Create formatters and add it to handlers
         ######################################## YOUR CODE HERE ##################################################
         # set the logging formatter to the f_handler
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        self.f_handler.setFormatter(formatter)
         ######################################## YOUR CODE HERE ##################################################
 
         ######################################## YOUR CODE HERE ##################################################
         # Add handlers to the logger and setlevel to DEBUG
+        self.logger.addHandler(self.f_handler)
+        self.logger.setLevel(logging.DEBUG)
         ######################################## YOUR CODE HERE ##################################################
 
     def warning(self, msg):
-        pass
+        self.logger.warning(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def error(self, msg):
-        pass
+        self.logger.error(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def info(self, msg):
-        pass
+        self.logger.info(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
     def debug(self, msg):
-        pass
+        self.logger.debug(msg)
         ######################################## YOUR CODE HERE ##################################################
         ######################################## YOUR CODE HERE ##################################################
 
 
 server_logger = Logger(log_file_name='server_logs.txt', module_name='server_logs')
 main_logger = Logger(log_file_name='main_logs.txt', module_name='main_logs')
-
-
-
